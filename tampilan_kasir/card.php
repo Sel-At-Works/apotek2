d7f60f383f38cad820be7119feb453a0ad4f346f
unknown <marselherlino3454@gmail.com>
2025-08-22 13:29:07 +0700

    penghilangan detail di card kasir

--- tampilan_kasir/card.php
+++ tampilan_kasir/card.php
@@ -19,10 +19,10 @@
     <div class="card kategori">
         <div class="icon">📦</div>
         <div class="card-info">
-            <h3>Total Laporan</h3>
+            <h3>Total Kategori</h3>
             <div class="count-row">
                 <p><?= $totalKategori; ?></p>
-                <a href="kategori.php" class="view-details">Lihat</a>
+                <!-- <a href="kategori.php" class="view-details">Lihat</a> -->
             </div>
         </div>
     </div>
